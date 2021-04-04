16a30e5e06bd2e47515edd5cc70fd4c9a620e4c5
Dan Chiev <danchiev2@Gmail.com>
2021-04-04 13:28:20 -0400

    updated app.js with routes

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
+var cors = require("cors");
+
+// routes
+const books = require("./routes/api/books");
+app.use("/api/books", books);
 const app = express();
+
 connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
